c14e1eb2a8468edbebcd04863654ea91cd93e6b2
Hasan Al Mamun <hihasan07@gmail.com>
2023-10-18 17:53:36 +0600

    Design List + Details Compose Functions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "xyz.hihasan.quotes"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "xyz.hihasan.quotes"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/xyz/hihasan/quotes/ui/screens/QuoteListItem.kt
+++ app/src/main/java/xyz/hihasan/quotes/ui/screens/QuoteListItem.kt
@@ -2,32 +2,34 @@ package xyz.hihasan.quotes.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.FormatQuote
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview
+
 @Composable
 fun QuoteListItem() {
     Card(
@@ -39,7 +41,7 @@ fun QuoteListItem() {
             modifier = Modifier.padding(16.dp)
         ) {
             Image(
-                imageVector = Icons.Filled.Close,
+                imageVector = Icons.Filled.FormatQuote,
                 colorFilter = ColorFilter.tint(Color.White),
                 alignment = Alignment.TopStart,
                 contentDescription = "Quotes",
@@ -61,7 +63,7 @@ fun QuoteListItem() {
                 Box(
                     modifier = Modifier
                         .background(Color(0xFFEEEEEE))
-                        .fillMaxWidth(.4f)
+                        .fillMaxWidth(.5f)
                         .height(1.dp)
 
                 )
@@ -75,4 +77,58 @@ fun QuoteListItem() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun QuoteDetails() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize(1f)
+            .background(
+                Brush.sweepGradient(
+                    colors = listOf(
+                        Color(0xFFffffff),
+                        Color(0xFFE3E3E3)
+                    )
+                )
+            )
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+            modifier = Modifier.padding(32.dp)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(16.dp, 24.dp)
+            ) {
+                Image(
+                    imageVector = Icons.Filled.FormatQuote,
+                    contentDescription = "Quote",
+                    modifier = Modifier
+                        .size(80.dp)
+                        .rotate(180F)
+                )
+
+                Text(
+                    text = "Time is the most value able thing a man can spend.",
+                    style = MaterialTheme.typography.titleMedium,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "- Theophrastus",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Thin
+                )
+
+
+
+            }
+
+        }
+    }
+
 }
